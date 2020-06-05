0fb36756598bdbfd7ec746e8bb7436486cd0d876
tanrongjun <1290460293@qq.com>
2020-06-05 10:54:33 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  async mounted() {
+    const result = await this.$api.reqList()
+    console.log(result)
+  },
+}
+</script>
+
+<style></style>
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    https: false,
+    open: true,
+    proxy: {
+      '/api': {
+        // 匹配所有以 '/api'开头的请求路径
+        target: 'http://localhost:5000', // 代理目标的基础路径
+        changeOrigin: true, // 支持跨域
+        pathRewrite: {
+          // 重写路径: 去掉路径中开头的'/api'
+          '^/api': '',
+        },
+      },
+    },
+  },
+}
